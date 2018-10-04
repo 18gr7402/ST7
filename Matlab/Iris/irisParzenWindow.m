be955d46b030fab8813382f7c32ad9ccf682235d
DESKTOP-ASPUCPF\Mette Tøttrup Gade <mtg15@student.aau.dk>
2018-10-04 09:46:05 +0200

    Slette oprindelige Parzen Window script, omdøbt kopieret matlab script til irisParzenWindow, samt rettet i koden og skrevet kommentarer til de forskellige linjer kode

--- Matlab/Iris/irisParzenWindow.m
+++ Matlab/Iris/irisParzenWindow.m
@@ -1,32 +1,57 @@
+%OBS: Dette kode tager 90% udgangspunkt i Alex' besvarelse fra PR 
+%kursusgang 3. Har ændret til vores iris data og værdier for n og
+%width der passer til det samt udvalgt en feature af de fire tilgængelige
+
 clc
 clear all
 close all
 
 load('irisData.mat');
 
-iris1 = [iris.sepal_length];
-traIris1 = iris1';
+iris1 = [iris.sepal_length]; %vælger en feature, sepal længde 
+%(næste skridt udover klassifier er nok at få alle fire med
+traIris1 = iris1'; 
 
-% Vi ved vi skal lave Parzen windows, hvorfor vi indsætter følgende kode,
-% som er en funktion Alex har lavet til MM3 i PR.
 
-%Histogram over data fordelt på de fire features. Data er ikke opdelt i
+%Histogram over data fordelt på en feature. Data er ikke opdelt i
 %klasser.
 figure(1)
 hist(iris1,20)
 title('Histogram over feature 1 data fordelt på de tre klasser')
 xlabel('Sepal length [cm]')
 ylabel('Count')
+axis([4 8 0 18])
 legend('Sepal length')
 
-% Ud fra histogrammet ses det, at datasættet ligger mellem værdierne cirka 4 og
-% 8, hvorfor vi nu ved, at vores Parzen Window skal dække over disse
-% værdier. For at være sikre på at dække 4-8 vælger vi at vinduet skal
-% dække lidt længere ud, hvorfor vi vælger fra 2-12.
+nr=[30 60 90 120 150]; %antal samples vi bruger
+width=[0.25 1 4]; % width svarer til h1, som er en udglatningsparameter, der anvendes til at beregne hn i funktionen Parzen.m
 
-featVal = (2:0.1:12);  % Området vores Parzen window skal bevæge sig indenfor
-featValNo = (1:length(featVal));
-data = traIris1(1:end); %Vælger antal samples, og her vælger vi alle fra feature 1
-nr = 150;   % Vælger antal samples man vil bruge til at lave sin model ud fra
-width = 0.5;    % Bredde på Parzen Window, manuelt bestemt
-pdfVal(featValNo) = parzen(featVal,data,nr,width)
+figure(2)
+n=1;
+featVal = (4:0.1:10); %laver vindue fra x=4 til x=10 der rykkes 0,1 af gangen
+for winWidthNo=1:length(width)
+    for sampAntNo=1:length(nr)
+        selectDat = traIris1(1:nr(sampAntNo));        
+        for featValNo =1:length(featVal) % Definere antal vindue vi beregner udfra. Der er ialt 101 vinduer.
+            % Da parzen funktionen laver et normalfordelt vindue (Gaussisk) bliver vinduet ikke kvadratisk men klokkeformet.
+            pdfVal(featValNo) =  parzen(featVal(featValNo),selectDat,nr(sampAntNo),width(winWidthNo)); %Sandsynlighedsværdien pn(x) for hvert af de 101 vinduer
+        end        
+%         w = pdfVal';
+        subplot(3,5,n)
+        plot(featVal,pdfVal)
+        axis tight
+        title(['h1 = ',num2str(width(winWidthNo)),' n = ',num2str(nr(sampAntNo))])
+        n=n+1;
+    end
+end
+xlabel('Feature value')
+ylabel('p(x)')
+
+%Ser på figur hvad der passer bedst ved at sammenligne fig 1 (histogram) 
+%med de enkelte grafer på fig 2. Kan også prøve med andre vinduestørrelser.
+%Gøres bare ved at ændre width
+
+%Forslag til næste skridt i dette kode:
+    %Bruge det til klassificering (alle n kan ikke bruges til at lave modellen opdel i træningsdata og testdata)
+    %Få alle fire features med
+
