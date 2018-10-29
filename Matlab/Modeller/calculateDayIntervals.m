628c8c226d094911be552c4ee56285b373ed461e
Bo Hansen <bwha15@student.aau.dk>
2018-10-29 14:53:32 +0100

    Lavet script til at opdele i dage

---
+++ Matlab/Modeller/calculateDayIntervals.m
@@ -0,0 +1,46 @@
+clc
+close all
+
+
+%% Opdeling af glukosemålinger på dage
+% load data
+
+
+%% Fjern labesl
+
+patient=[hypodiabetesglukoseunitAdmitlabOffset.patientunitstayid];   %Tilpas til data (med eller uden 'no')
+labresult=[hypodiabetesglukoseunitAdmitlabOffset.labresult];
+offset = [hypodiabetesglukoseunitAdmitlabOffset.labresultoffset];
+unitAdmitTime = [hypodiabetesglukoseunitAdmitlabOffset.unitadmittime24];
+
+% format3 = 'HH:MM:SS';
+% DateNumber3 = datenum(DateString,format3)sortedmat = sortrows(mat, 1);
+[~,idu] = unique(hypodiabetesglukoseunitAdmitlabOffset(:,1));
+uniqueRows = hypodiabetesglukoseunitAdmitlabOffset(idu,:);
+
+%% Fjern patienter der kun har målinger for 1 døgn
+
+%% Create struct
+
+%% Find unikke
+[u] = unique(patient);
+uniquePatient = u;
+
+for index=1:length(uniquePatient)
+    timeIndex = uniqueRows(index,4);
+    [h,m] = hms(timeIndex.unitadmittime24);
+    tidIMin = 60*h+m;
+    tidTilMidnat = 1440-tidIMin;
+    
+    n=find(uniquePatient(index) == patient);
+    labTest(index).PatientID = uniquePatient(index);
+    labTest(index).( ['Day',num2str(1)]).('var') = find(offset(n)<tidTilMidnat);
+
+    numberOfTestDays = ceil((max(offset(n))-tidTilMidnat)/1440);
+    
+    for i=0:numberOfTestDays-1
+        labTest(index).( ['Day',num2str(i+2)]).('var') = find((i*1440+tidTilMidnat) <= offset(n) & (i*1440+tidTilMidnat+1440) > offset(n));
+    end
+    index = index + 1;
+    
+end
