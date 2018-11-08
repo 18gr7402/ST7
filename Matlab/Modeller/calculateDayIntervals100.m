33513e3e357d4524622e40e0c91ed305f860419c
Bo Hansen <bwha15@student.aau.dk>
2018-11-08 09:56:54 +0100

    Tilføjet calculateDayInterval100

--- Matlab/Modeller/calculateDayIntervals100.m
+++ Matlab/Modeller/calculateDayIntervals100.m
@@ -1,28 +1,26 @@
 clc
 close all
 
-
 %% Opdeling af glukosemålinger på dage
 % load data
 
 %% Fjern labels
 
-patient=[hypodiabetesglukoseunitAdmitlabOffset.patientunitstayid];
-labresult=[hypodiabetesglukoseunitAdmitlabOffset.labresult];
-offset = [hypodiabetesglukoseunitAdmitlabOffset.labresultoffset];
+patientID=[nohypodiabetesglukoseunitAdmitlabOffset.patientunitstayid];
+labResult=[nohypodiabetesglukoseunitAdmitlabOffset.labresult];
+offset = [nohypodiabetesglukoseunitAdmitlabOffset.labresultoffset];
 
 %% Find unikke rows af patientid med tilhørende tid
-[~,idu] = unique(hypodiabetesglukoseunitAdmitlabOffset(:,1));
-uniqueRows = hypodiabetesglukoseunitAdmitlabOffset(idu,:);
+[~,idu] = unique(nohypodiabetesglukoseunitAdmitlabOffset(:,1));
+uniqueRows = nohypodiabetesglukoseunitAdmitlabOffset(idu,:);
 
 %% Find unikke rows af patient uden labes så det kan bruges til beregning
-[u] = unique(patient);
+[u] = unique(patientID);
 uniquePatient = u;
 
 %% Løkke til opdeling af målinger for hver unikke patientid
-
 % Preallocate
-testDay = zeros(length(labresult),1);
+testDay = zeros(length(labResult),1);
 
 for index=1:length(uniquePatient)
     timeIndex = uniqueRows(index,4);  % Find admit-time for patient 'index' 
@@ -30,20 +28,17 @@ for index=1:length(uniquePatient)
     tidIMin = 60*h+m;
     tidTilMidnat = 1440-tidIMin;
     
-    n=find(uniquePatient(index) == patient); % Find de samples der tilhøre patient 'index'
+    n=find(uniquePatient(index) == patientID); % Find de samples der tilhøre patient 'index'
             
     % Udregning af hvor mange dage patienten har data for. Der findes offset for den sidste måling (ved max(offset(n))). Dette divideres med 60*24 og rundes op.
     numberOfTestDays = ceil((max(offset(n))-tidTilMidnat)/1440);
     
-    % Løkke for opdeling af Day2 indtil antallet af dag med test.
+    % Løkke for opdeling af dag 1 indtil antallet af dag med test. De der hører til dag 0 er allerede 0.
     for i=0:numberOfTestDays-1
     % Der findes de samples hvor patientens offset ligger over tidTilMidnat og under tidTilMadnat+(60*24). For hver iteration ligges i*(60*24) oveni begge for på den måde at skrifte til en ny dag. Dette gemmes hver gang på som dag(i+1).
         testDay(n(find((i*1440+tidTilMidnat) <= offset(n) & (i*1440+tidTilMidnat+1440) > offset(n))))=i+1;
     end
-    
-    index = index + 1;
 end
 
 % Vi slutter med at samle data.
-
-allData = table(patient,labresult,offset,testDay);
+allGlucoseDataNoHypo = table(patientID,labResult,testDay);
